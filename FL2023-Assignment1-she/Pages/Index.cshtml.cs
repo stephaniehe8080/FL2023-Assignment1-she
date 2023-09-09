d660574710bb4ccd402160392e7ff7171a80ee55
steph <steph@DESKTOP-78JOI4I>
2023-09-09 01:58:11 -0500

    Add project files.

---
+++ FL2023-Assignment1-she/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FL2023_Assignment1_she.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
